2d3fe09572df4c2eba085e403fc07e00575ddba5
Christian Coleman <christian.c2508@gmail.com>
2018-03-29 01:03:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function createMultiplier(multiplierValue){
 
 var doubler = createMultiplier(2);
 var tripler = createMultiplier(3);
+
+function multiplier(multiplierValue, value){
+  return function(){
+    return multiplierValue * value;
+  }
+}
+
+var doublerWithBind = multiplier(2, value.bind(null));
+var triplerWithBind = multiplier(3, value.bind(null));
