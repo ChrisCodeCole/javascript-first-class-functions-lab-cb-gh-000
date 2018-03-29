186fc6e77bcca3764980e8d8140ebc60726a5be9
Christian Coleman <christian.c2508@gmail.com>
2018-03-29 00:46:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ function countdown(secondFunction){
   window.setTimeout(secondFunction, 2000);
 }
 
-function doubler(num){
-  num = num * 2;
-  return num;
-}
-function tripler(num){
-  num = num * 3;
-  return num;
-}
-function createMultiplier(multiplierValue, number){
-    if(multiplierValue == 2){return doubler(number)}
-    else{return tripler(number)}
+var doubler;
+var tripler;
+
+function createMultiplier(multiplierValue, ){
+  return function(doubler,tripler){
+    doubler = doubler * 2;
+    tripler = tripler * 3;
+  }
 }
